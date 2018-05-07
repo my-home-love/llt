c9fdc70b1046958433b677eb9d789bdb8d60fd4c
llt <1171551241@qq.com>
2018-05-07 18:13:11 +0800

    add learning1

--- dist/llt.cfb87bbe.js
+++ dist/llt.cfb87bbe.js
@@ -100,7 +100,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({6:[function(require,module,exports) {
 
-},{}],13:[function(require,module,exports) {
+},{}],12:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -129,7 +129,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '4603' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '36181' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -270,4 +270,4 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[13,6], null)
+},{}]},{},[12,6], null)
